2c5b7ce48c1bafafe86ccddd1f31f86e658e913b
root <maxxamillion04@gmail.com>
2020-07-01 06:58:14 -0500

    spam filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,276 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Slider from "@material-ui/core/Slider";
+//import { filterSpam } from "./filterSpam.js";
+import { CircularProgress } from "@material-ui/core";
+import filterSpam, {resetFilter, assignProbability} from "./spamFilter";
 
 function App() {
+  const [emailArray, updateEmailArray] = useState([]);
+  const [spamValue, setSpamValue] = useState(70);
+  const [spamBucketCount, setSpamBucketCount]=useState(5);
+
+  //const SpamFilter=require('./spamFilter');
+  /*
+  this useEffect block populates the table with samples from each email file.
+  It is called on the initial render of the page
+  It also acts as a scanner to determine how many email files we have
+
+  This is just for the UI of this, the actual filter code lives in filterSpam.js
+
+  */
+  useEffect(async () => {
+    var newEmailArray = [];
+    var moreFiles = true;
+    for (var x = 0; moreFiles; x++) {
+      var header, sample;
+      var file = await fetch(`/emails/email${x}.txt`)
+        .then((r) => r.text())
+        .then((text) => {
+
+          var lines = text.split("\n");
+
+          //this is what terminates our for loop, and stops the scan for more email files
+          // If there is no file at the address being fetched,
+          // JS will automatically fetch a template HTML file with this as the first line
+          // when that happens, we are done.
+          if (lines[0] === "<!DOCTYPE html>") {
+            moreFiles = false;
+          } else {
+            //this will catch if the file isnt formatted right. Bucket===0 will be used to prevent that file from being scanned later on
+            if (lines[0] !== "header:") {
+              console.log("This email file is not in the correct format:"+x);
+
+              newEmailArray[x] = {
+                header: "Could not read file!",
+                bodySample: "error",
+                charLength: 0,
+                score: 100,
+                bucket: -1,
+                probability: 100,
+                scored: true,
+              };
+            } else {
+              //if the file is formatted correctly, strip a sample of the data!
+              var head = lines[1].substring(0, 24);
+              if (lines[1].length > 24) {
+                head += "...";
+              }
+              var bodySample;
+              if (lines[3] && lines[3].length > 16) {
+                bodySample = lines[3].substring(0, 16) + "...";
+              } else {
+                if (lines[3]) bodySample = lines[3];
+                else bodySample = "#error#";
+              }
+
+              //character length of just the body means subtracting the length of the header and placeholders for "header" and "body"
+              var charL =
+                text.length -
+                lines[0].length -
+                lines[1].length -
+                lines[2].length;
+              newEmailArray[x] = {
+                header: head,
+                sample: bodySample,
+                charLength: charL,
+                score: 0,
+                bucket: "N/A",
+                probability: 0,
+                scored: false,
+              };
+            }
+          }
+        });
+      updateEmailArray([...newEmailArray]);
+    }
+  }, []);
+
+/*
+debugging code  
+  useEffect(()=>{
+    console.log(emailArray);
+  },[emailArray]);
+*/
+  /*
+  this creates the components for the UI table, which displays the results of the SpamFilter
+  */
+  function populateEmailTable() {
+    if (emailArray.length === 0) {
+      return (
+        <tr>
+          <td>
+            <CircularProgress />
+          </td>
+          <td>
+            <CircularProgress />
+          </td>
+          <td>
+            <CircularProgress />
+          </td>
+          <td>
+            <CircularProgress />
+          </td>
+          <td>
+            <CircularProgress />
+          </td>
+          <td>
+            <CircularProgress />
+          </td>
+        </tr>
+      );
+    }
+    // var tableData="";
+
+    return emailArray.map((emailRow, key) => {
+      //var thisColor = emailRow.score < spamValue ? "green" : "red";
+
+      var isit=(function(){
+        if(emailRow.probability<50)
+          return "NOPE";
+          if(emailRow.probability <80)
+          return "POSSIBLY"
+          if(emailRow.probability>=80)
+          return "YEP"
+      })();
+      return (
+        <tr key={key}>
+          <td>{emailRow.header}</td>
+          <td>{emailRow.sample}</td>
+          <td>{emailRow.charLength}</td>
+          <td>{emailRow.score}</td>
+          <td>{emailRow.bucket}</td>
+          <td>{emailRow.probability}%</td>
+          <td>
+            
+              {emailRow.scored
+                ? isit
+                : "NOT FILTERED YET!"}
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  const handleSliderChange = (event, newValue) => {
+    setSpamValue(newValue);
+  };
+
+  const handleSpamBucketCountSliderChange = (event, newValue) =>{
+    setSpamBucketCount(newValue);
+  }
+
+/*  async function makeItWait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}*/
+
+
+  
+  async function activateSpamFilter (e) {
+    resetFilter(spamValue,spamBucketCount);
+    e.preventDefault();
+    var newEmailArray=[];
+
+    Promise.all(emailArray.map( async function(emailRow,index){
+
+      if (emailRow.bucket !== -1) {
+          
+          var newEmailVals= await filterSpam(`/emails/email${index}.txt`);
+          emailRow.bucket=newEmailVals.bucket;
+          emailRow.score=newEmailVals.score;
+          emailRow.scored=true;
+
+          
+          newEmailArray[index]=emailRow;
+
+          
+
+        }else{
+          newEmailArray[index]=emailRow;
+        }    
+
+    })).then(()=>{
+
+      Promise.all(newEmailArray.map( async function(emailRow,index){
+        if(emailRow.bucket!==-1){
+          emailRow.probability= await assignProbability(emailRow.bucket,emailRow.score);
+
+          newEmailArray[index]=emailRow;
+        }
+      })).then(()=>{
+
+      updateEmailArray(newEmailArray);
+      }
+      ).catch((err)=> console.log(err));
+    });    
+    
+  }
+
+  const scoreMarks=[
+    {value: 0,
+    label:"0"},
+    {value:100,
+    label:"100"},
+    {value: 70,
+    label:"70"}
+  ];
+  const similarMarks=[
+    {value: 0,
+      label:"0"},
+      {value:10,
+      label:"10"},
+      {value: 5,
+      label:"5"}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th>Header</th>
+              <th>Body Sample</th>
+              <th>Length</th>
+              <th>Similarity Score</th>
+              <th>Spam Bucket</th>
+              <th>Spam Probability</th>
+              <th>Is It Spam?</th>
+            </tr>
+          </thead>
+          <tbody>{populateEmailTable()}</tbody>
+        </table>
+        <Button onClick={(event)=>activateSpamFilter(event)} variant="warning" size="lg">FILTER THESE EMAILS!</Button>
+        <span>How similar is too similar?</span>
+        <Slider
+          className="slider"
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          value={spamValue}
+          step={5}
+          marks={scoreMarks}
+          min={0}
+          max={100}
+          onChange={handleSliderChange}
+        />
+        <span>How many similar emails constitutes "Spam"?</span>
+        <Slider
+          className="slider"
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          value={spamBucketCount}
+          step={1}
+          marks={similarMarks}
+          min={0}
+          max={10}
+          onChange={handleSpamBucketCountSliderChange}
+        />
+        <br />
+        <br />
+        <br />
       </header>
     </div>
   );
---
+++ src/spamFilter.js
@@ -0,0 +1,155 @@
+const commonWords = ["hello", "goodbye", "greetings", "sincerely"];
+var buckets = [];
+
+var spamThreshold = 70;
+var spamCount = 5; //number of similar emails needed to constitute "Spam"
+
+/*
+here is the actual spam filtering/bucketizing logic
+The process is as thus:
+1. get the body text from the file
+2. remove all symbols and excessive whitespace to compare only alphanumeric words, solely for the sake of logical simplicity
+3. strip the first 400 characters of the email to compare contents. This could be changed to be more if it was determined to be a point of weakness
+4. add all words from the body into a hashset, which is a set of all unique words in the email
+//the contents are then checked against all previous data that has been processed, and bucketized:
+5. Scoring part 1: how similar the words of this email are versus previous filtered emails, which have been sorted into "buckets"
+6. Scoring part 2: how does the length of this email compare to the original email of the bucket? If it is not within 30%, the score is reduced, by up to half
+//done scoring
+7. the greatest score among all buckets is checked against the "spamValue" threshold, which is passed in from the App(70% is the default, similar enough to be considered spam)
+7a. if the score is greater than the threshold, the email is put in the same bucket and is possibly spam.
+7b. if the score is below threshold, the email gets its own "bucket" to be checked against future 
+8. finally, the bucket email count is incremented, and the score and bucket # are passed back to the app
+*/
+export default async function filterSpam(filePath) {
+  //init
+  var score = 0;
+  var bucket = 0;
+  var emailSet = new Set();
+  //1. get the body text from the file
+  var file = await fetch(filePath)
+    .then((r) => r.text())
+    .then((text) => {
+      var bodyText = text.split("\n");
+      bodyText.shift();
+      bodyText.shift();
+      bodyText.shift();
+
+      bodyText = bodyText.join(" ");
+      //now bodyText contains only the body of the email
+
+      //2. removing all non-alphanumeric characters and whitespace
+      bodyText = bodyText.replace(/[^A-Za-z0-9]/gim, " ").replace(/\s+/g, " ");
+
+      //save the length of our body for scoring in step 6 later
+      var bodyLength=bodyText.length;
+
+      //3. we are only going to check the first 400 characters of each email.
+      //If their first 400 characters don't match significantly enough,
+      //its not likely the rest of the contents are the same either
+      //Note: This could be changed if it was determined to be a point of weakness, but currently optimizes for computation time using a reduced dataset
+      bodyText.substring(0, 400);
+
+      //4. split the text into an array of words. all words will be separated by one space due to the regex in step 2
+      bodyText = bodyText.split(" ");
+      //add all words to the Set
+      bodyText.forEach((word, index) => {
+        emailSet.add(word);
+      });
+
+
+      var maxScore = 0;
+      var maxBucketIndex = 0;
+      
+      /*5.
+      Get initial score, by taking the set of words from the email, taking the size, then removing all the words from the bucket word set
+      the score is a ratio of endSize/startSize, so remaining words / starting words, then scaled up to be a percentage out of 100.
+      */
+      if (buckets.length !== 0) {
+        buckets.forEach((bucket, index) => {
+          var thisScore = 0;
+          //emailSet is copied, and left intact, to be used for generating a bucket in step 7b below
+          var copySet = new Set(emailSet);
+          var startSize = copySet.size;
+
+          //delete all similar words found in the bucket word set
+          bucket.wordSet.forEach((word) => {
+            copySet.delete(word);
+          });
+          var endSize = copySet.size;
+
+          //calculate the ratio, scale to 100, take the difference out of 100 to get % similarity
+          thisScore = 100 - (100 * endSize) / startSize;
+
+          
+
+        /* 6. modify score by length
+        //if the email is not within 30% the length of the buckets email length, there is a score reduction, up to half, 
+        //since they are deemed to be not so similar
+        reasoning: there may be highly variable length spam emails, but I dont want them to catch non-spam emails unnecessarily
+        if someone submitted an email that was a complete English dictionary, and it was bucketized, it could flag all following emails as spam,
+        therefore, I determined that the length of the email needs to be factored into the score as well
+        */
+        var bucketLength = bucket.bodyLength;
+          if (
+            !(
+              bodyLength <= bucketLength * 1.3 &&
+              bodyLength >= bucketLength * 0.7
+            )
+          ) {
+            thisScore =
+            bodyLength > bucketLength
+                ? thisScore*(0.5 + (0.5 * (bucketLength / bodyLength)))
+                : thisScore*(0.5 + (0.5 * (bodyLength / bucketLength)));
+          }
+
+
+          /*get max score among all buckets, and keep track of which bucket index scored highest*/
+          if (thisScore > maxScore) {
+            maxScore = thisScore;
+            maxBucketIndex = index;
+          }
+        });
+
+        if (maxScore < spamThreshold) {
+          //not similar enough to any current buckets to live in the same bucket.
+          var newBucket = { wordSet: emailSet, bodyLength:bodyLength, emails: 1 };
+          buckets.push(newBucket);
+          maxBucketIndex = buckets.length-1;
+        } else {
+          //It fits in a bucket! increase bucket email count
+
+          buckets[maxBucketIndex].emails++;
+
+          //bucket.wordSet.add();
+        }
+      } else {
+        //there are no buckets yet, so the first email gets its own
+        var newBucket = { wordSet: emailSet, emails: 1 };
+        buckets.push(newBucket);
+        maxBucketIndex = 0;
+      }
+
+      score = Math.round(100 * maxScore) / 100;
+      bucket = maxBucketIndex;
+    });
+
+  return { bucket: bucket, score: score };
+}
+
+/*
+Here is where the probability of spam is assigned.
+The logic here is simple: if there are too many emails in a bucket, they should ALL be flagged as spam
+20% of probability is similarity score, calculated during bucketization above, 80% is determined by # of emails in the bucket
+It is just a flat ratio of emails in bucket / (number of similar emails needed to flag something as spam) or 100
+*/
+export function assignProbability(bucketNum,score) {
+  console.log(buckets[bucketNum].emails+" "+ spamCount);
+  var prob= 20*(score/100) + 80*(buckets[bucketNum].emails / spamCount);
+  return prob>100? 100:prob;
+}
+
+export function resetFilter(spamScore, spamC) {
+  buckets = [];
+  spamThreshold = spamScore;
+  spamCount = spamC;
+}
